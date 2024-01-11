dea24eb4414e028a42745b5bc0e0d914cacf1d8b
Brandon Boudreaux <hf3209@wayne.edu>
2024-01-11 17:01:08 -0500

    Custom constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,21 +46,21 @@ public final class Constants {
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
     // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-    public static final double kFrontRightChassisAngularOffset = 0;
-    public static final double kBackLeftChassisAngularOffset = Math.PI;
-    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+    public static final double kFrontLeftChassisAngularOffset = Math.toRadians(137.6);
+    public static final double kFrontRightChassisAngularOffset = Math.toRadians(281.71);
+    public static final double kBackLeftChassisAngularOffset = Math.toRadians(39.7);
+    public static final double kBackRightChassisAngularOffset = Math.toRadians(70.9);
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 11;
-    public static final int kRearLeftDrivingCanId = 13;
-    public static final int kFrontRightDrivingCanId = 15;
-    public static final int kRearRightDrivingCanId = 17;
+    public static final int kFrontLeftDrivingCanId = 2;
+    public static final int kRearLeftDrivingCanId = 3;
+    public static final int kFrontRightDrivingCanId = 1;
+    public static final int kRearRightDrivingCanId = 4;
 
-    public static final int kFrontLeftTurningCanId = 10;
-    public static final int kRearLeftTurningCanId = 12;
-    public static final int kFrontRightTurningCanId = 14;
-    public static final int kRearRightTurningCanId = 16;
+    public static final int kFrontLeftTurningCanId = 22;
+    public static final int kRearLeftTurningCanId = 33;
+    public static final int kFrontRightTurningCanId = 11;
+    public static final int kRearRightTurningCanId = 44;
 
     public static final boolean kGyroReversed = false;
   }
